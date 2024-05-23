3488e3e5f5160bdbb050ba38a9ad1abdc138afb1
SamarthKuchya <samarthkuchya@gmail.com>
2024-05-24 00:30:43 +0530

    changed

--- sentiment/settings.py
+++ sentiment/settings.py
@@ -120,8 +120,8 @@ STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build','static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 
 
